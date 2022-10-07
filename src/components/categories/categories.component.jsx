9df5d27e0b818f9d13552921d6a8f3470335fccc
Frank Benady <knarfben110@gmail.com>
2022-10-07 23:53:17 +0200

    wip

--- src/components/categories/categories.component.jsx
+++ src/components/categories/categories.component.jsx
@@ -5,7 +5,6 @@ const Categories = ({ categories }) => {
   return (
     <div className="categories-container">
       {categories.map((c) => {
-        console.log({ category: c });
         return <CategoryItem key={c.id} category={c} />;
       })}
     </div>
